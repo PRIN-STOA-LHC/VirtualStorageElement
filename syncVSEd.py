a8ae109015d4c831799f3fe5fdeee263b1794e5d
svallero <svallero@to.infn.it>
2014-05-23 17:07:07 +0200

    Added input options + config file.

--- syncVSEd.py
+++ syncVSEd.py
@@ -24,19 +24,6 @@
 #                                                                             #
 ###############################################################################
 
-# Configuration goes here ##################################################### 
-logfile="/tmp/syncVSEd.log"
-pidfile="/tmp/syncVSEd.pid"
-# time given to notifier to read events
-inotify_timeout=10
-# sleep time for main loop
-sleep_time=10 
-watched_dir="/vse"
-local_se="ALICE::Torino::SE"
-alien_user="svallero"
-se_posix_path="/glfs/alice-xrd/xrootd"
-###############################################################################
-
 # Standard python libs
 import logging
 import time
@@ -44,18 +31,15 @@ import sys
 import os
 import subprocess
 from subprocess import Popen, PIPE
+from ConfigParser import SafeConfigParser
 
 # Third party libs
 from daemon import runner
 import pyinotify
 
-# Logger ----------------------------------------------------------------------
-logger = logging.getLogger("syncVSEdLog")
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-lhandler = logging.FileHandler(logfile)
-lhandler.setFormatter(formatter)
-logger.addHandler(lhandler)
+# Global vars
+global logfile
+logfile = '/dev/null'
 
 # Daemon class, main loop is defined here -------------------------------------
 class App():
@@ -64,15 +48,15 @@ class App():
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
-        self.pidfile_path =  pidfile
         self.pidfile_timeout = 5
-            
+        self.pidfile_path = '/tmp/syncVSEd.pid' 
+    
     def run(self):
         logger.info("********************************")
         logger.info("*** Starting daemon syncVSEd ***")
         logger.info("********************************")
         # source AliEn environment (CVMFS style)
-        command = ('source /cvmfs/alice.cern.ch/etc/login.sh && eval $( alienv printenv VO_ALICE@ROOT::v5-34-13 )')
+        command = ('source /cvmfs/alice.cern.ch/etc/login.sh && eval $( alienv printenv VO_ALICE@ROOT::'+root_version+' )')
         run_shell_cmd(command)
 	# Inotify stuff: 
 	# watch manager
@@ -82,9 +66,9 @@ class App():
 	# event handler
 	ihandler = EventHandler()
 	# notifier
-	notifier = pyinotify.Notifier(wm,ihandler,timeout=inotify_timeout)
+	notifier = pyinotify.Notifier(wm,ihandler,timeout=int(inotify_timeout))
 	# add dir to watch recursively
-	wm.add_watch(watched_dir, mask, rec=True)
+	wm.add_watch(watched_dir, mask, rec=True, auto_add=True)
          
         # MAIN LOOP
         while True:
@@ -96,7 +80,113 @@ class App():
             symlinks_check()  
             # Sleep   
             logger.info('Sleeping '+str(sleep_time)+'s...')
-            time.sleep(sleep_time)
+            time.sleep(int(sleep_time))
+
+# Configuration ---------------------------------------------------------------
+def configure(config_file):
+   if not config_file:
+      print "==> Configuration file not specified."
+      load_defaults()
+   else:
+       print "==> Reading configuration file..."
+       cf_parser = SafeConfigParser()
+       if len(cf_parser.read(config_file)) == 0:
+          print ('Cannot read configuration file: %s!' % config_file)
+          sys.exit(1)
+       for key,val in cf_parser.items('vse'): 
+         globals() [key]=val
+          
+
+def load_defaults():
+   print "==> Loading default configuration..."
+   # log-file
+   global logfile
+   logfile="/tmp/syncVSEd.log"
+   print '..... logfile='+logfile+''
+   # time given to notifier to read events
+   global inotify_timeout
+   inotify_timeout=10
+   print '..... inotify_timeout='+str(inotify_timeout)+''
+   # sleep time for main loop
+   global sleep_time 
+   sleep_time=10 
+   print '..... sleep_time='+str(sleep_time)+''
+   # watched dir
+   global watched_dir
+   watched_dir="/vse"
+   print '..... watched_dir='+watched_dir+''
+   # local SE
+   global local_se
+   local_se="ALICE::Torino::SE"
+   print '..... local_se='+local_se+''
+   # alien user (certificate key should not be encripted=no password)
+   global alien_user
+   alien_user="svallero"
+   print'..... alien_user='+alien_user+''
+   # local posix path to mounted storage
+   global se_posix_path
+   se_posix_path="/glfs/alice-xrd/xrootd"
+   print '..... se_posix_path='+se_posix_path+''
+   # root version
+   global root_version
+   root_version="v5-34-13"
+   print '..... root_version='+root_version+''
+ 
+# Logger ---------------------------------------------------------------------- 
+def define_logger(level,logfile):
+   global logger
+   logger = logging.getLogger("syncVSEdLog")
+   if level == 'DEBUG':
+      logger.setLevel(logging.DEBUG)
+   if level == 'INFO':
+      logger.setLevel(logging.INFO)
+   if level == 'ERROR':
+      logger.setLevel(logging.ERROR)
+   formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+   global lhandler
+   lhandler = logging.FileHandler(str(logfile))
+   lhandler.setFormatter(formatter)
+   logger.addHandler(lhandler)
+
+# Custom daemon runner --------------------------------------------------------
+class MyDaemonRunner(runner.DaemonRunner):
+    def __init__(self, app):
+        # workaround...
+        self.app_save = app
+ 
+        self.detach_process = True
+        runner.DaemonRunner.__init__(self, app)
+ 
+    def parse_args(self, argv=None):
+        # Note that DaemonRunner implements its own parse_args(), and
+        # it is called in __init__ of the class.
+        # Here, we override it using argparse.
+        import argparse
+        log_level=''
+	parser = argparse.ArgumentParser('syncVSE')
+        parser.add_argument('-c', '--configfile', help='path to configuration file')
+        parser.add_argument('-l', '--loglevel', help='DEBUG|INFO|ERROR (default=INFO)')
+        parser.add_argument('action', help='start|stop|restart')
+        args = parser.parse_args()
+        # action
+        self.action =  args.action 
+        if self.action not in self.action_funcs:
+            self._usage_exit(sys.argv)
+        # log level
+        log_level=args.loglevel
+        if not log_level:
+           log_level='INFO'
+        if log_level not in ['DEBUG','INFO','ERROR']:  
+           print 'allowed log levels: DEBUG, INFO, ERROR'
+           sys.exit(1) 
+        # config file
+        config_file=args.configfile
+        if self.action != 'stop':
+           # apply configuration
+           configure(config_file)
+        # define logger
+        define_logger(log_level, logfile) 
+
 
 # Inotify stuff ---------------------------------------------------------------
 class EventHandler(pyinotify.ProcessEvent):
@@ -200,10 +290,14 @@ def run_shell_cmd(command):
          logger.debug(errline)
    except:
       logger.error('Running command '+command+'')
-  
+
+
+
+# ENTRY POINT #
 # Define daemon runner --------------------------------------------------------
 app = App()
-daemon_runner = runner.DaemonRunner(app)
+#daemon_runner = runner.DaemonRunner(app)
+daemon_runner = MyDaemonRunner(app)
 #This ensures that the logger file handle does not get closed during daemonization
 daemon_runner.daemon_context.files_preserve=[lhandler.stream]
 # Run daemon
