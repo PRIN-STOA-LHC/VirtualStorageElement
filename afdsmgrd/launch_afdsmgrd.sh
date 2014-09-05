e712aae2424ce83c2eba15a3c2c926013f979214
Dario Berzano <dario.berzano@cern.ch>
2014-09-05 16:27:10 +0200

    Use standard "service" command to control afdsmgrd

--- afdsmgrd/launch_afdsmgrd.sh
+++ afdsmgrd/launch_afdsmgrd.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-source /cvmfs/alice.cern.ch/etc/login.sh
-eval $( alienv printenv VO_ALICE@ROOT::v5-34-13 )
- 
-CONFIG_FILE="/home/vseadmin/afdsmgrd/config/afdsmgrd.conf"
-PID_FILE="/home/vseadmin/afdsmgrd/run/afdsmgrd.pid"
-DEBUG_LEVEL="high"
-LOG_FILE="/home/vseadmin/afdsmgrd/log/afdsmgrd.log"
-LIBEXEC_PATH="$ROOTSYS/etc/proof"
-# echo $LIBEXEC_PATH
-afdsmgrd -c $CONFIG_FILE -p $PID_FILE -d $DEBUG_LEVEL -l $LOG_FILE -e $LIBEXEC_PATH &
-
+# works also if not root
+exec /sbin/service afdsmgrd start
